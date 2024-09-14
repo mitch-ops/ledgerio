7ae925b60ae4323845d169fc5ffa1ffd15164693
Mitchell <mitch.heidbrink@gmail.com>
2024-09-14 15:51:15 -0400

    add invite link creation and configure button

--- components/group-details.tsx
+++ components/group-details.tsx
@@ -2,8 +2,22 @@ import React from "react";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
-import { PenSquare, Plus, ChevronRight } from "lucide-react";
+import { PenSquare, Plus, ChevronRight} from "lucide-react";
 import Link from "next/link";
+import { Copy } from "lucide-react"
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 export type Transaction = {
   id: number;
@@ -123,6 +137,43 @@ export default function GroupDetails({
       >
         <Plus className="h-6 w-6" />
       </Button>
+        
+      <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Invite</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Share link</DialogTitle>
+          <DialogDescription>
+            Anyone who has this link will be able to view this.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex items-center space-x-2">
+          <div className="grid flex-1 gap-2">
+            <Label htmlFor="link" className="sr-only">
+              Link
+            </Label>
+            <Input
+              id="link"
+              defaultValue="https://ui.shadcn.com/docs/installation"
+              readOnly
+            />
+          </div>
+          <Button type="submit" size="sm" className="px-3">
+            <span className="sr-only">Copy</span>
+            <Copy className="h-4 w-4" />
+          </Button>
+        </div>
+        <DialogFooter className="sm:justify-start">
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
     </div>
   );
 }
