badb47e39ae6fbce05729d45b9e926ce2dc37c3f
hessnt30 <hessnt30@vt.edu>
2024-09-14 20:31:00 -0400

    finished invites

--- app/join-group/[token]/page.tsx
+++ app/join-group/[token]/page.tsx
@@ -1,17 +1,21 @@
 "use client";
 
-import { notFound, redirect } from "next/navigation"; // To handle 404 errors
+import { notFound } from "next/navigation"; // To handle 404 errors
 import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Loader2 } from "lucide-react";
 import { createClient } from "@/utils/supabase/client";
+import Link from "next/link";
 
 const supabase = createClient();
 
 const JoinGroupPage = ({ params }: { params: { token: string } }) => {
   const token = params.token; // Get the invite ID (UUID) from the URL
   const [loading, setLoading] = useState(true);
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState(
+    "Successfully joined group!"
+  );
+  const [groupId, setGroupId] = useState("");
 
   useEffect(() => {
     const joinGroup = async () => {
@@ -42,12 +46,30 @@ const JoinGroupPage = ({ params }: { params: { token: string } }) => {
           return;
         }
 
-        // Add the user to the group membership
-        const { error: membershipError } = await supabase
-          .from("group_memberships")
-          .insert([
-            { user_id: userId, group_id: invite.group_id, role: "member" },
-          ]);
+        setGroupId(invite.group_id);
+
+        // Check if the user is already a member of the group before inserting
+        const { data: existingMembership, error: existingMembershipError } =
+          await supabase
+            .from("group_memberships")
+            .select("*")
+            .eq("user_id", userId)
+            .eq("group_id", invite.group_id)
+            .single();
+
+        if (existingMembership) {
+          setErrorMessage("User is already a member of this group");
+          setLoading(false);
+          return;
+        }
+
+        // If no existing membership, insert the new membership
+        const { data: insertMembershipData, error: membershipError } =
+          await supabase
+            .from("group_memberships")
+            .insert([
+              { user_id: userId, group_id: invite.group_id, role: "member" },
+            ]);
 
         if (membershipError) {
           setErrorMessage("Failed to join group");
@@ -55,8 +77,9 @@ const JoinGroupPage = ({ params }: { params: { token: string } }) => {
           return;
         }
 
-        // Redirect to the group page after successfully joining
-        redirect(`/groups/${invite.group_id}`);
+        setLoading(false);
+
+        //
       }
     };
 
@@ -64,10 +87,20 @@ const JoinGroupPage = ({ params }: { params: { token: string } }) => {
   }, [token]); // Watch for changes in the token
 
   return (
-    <Button disabled>
-      <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-      {loading ? "Please wait" : errorMessage}
-    </Button>
+    <>
+      <div className="w-full flex justify-center">
+        <div className="w-full flex justify-center items-center">
+          {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+          {loading ? "Please wait" : errorMessage}
+        </div>
+
+        {!loading && (
+          <Link href={`/groups/${groupId}`}>
+            <Button>Go to Group</Button>
+          </Link>
+        )}
+      </div>
+    </>
   );
 };
 
